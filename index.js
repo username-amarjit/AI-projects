98161c858d2155a77cf059992b3bce308014c444
amarjit <amarjit.yadav@bitsflowtech.com>
2025-01-14 00:07:27 +0530

    added axios call, and sample call

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ let CONFIG_DATA;
 document.addEventListener("DOMContentLoaded", renderEnvValues);
 
 async function renderEnvValues() {
-    CONFIG_DATA = await get_config()
+    CONFIG_DATA = await get_config();
     // .then((data) => {
     //     CONFIG_DATA = data;
     // });
-    console.log(CONFIG_DATA,'CONFIG_DATA');
     heading = document.querySelector(".heading");
-    heading.children[0].innerText = CONFIG_DATA.HEADING || "AI CHAT";
+    heading.children[0].children[0].innerText =
+        CONFIG_DATA.HEADING || "AI CHAT";
+    heading.children[0].children[1].innerText =
+        CONFIG_DATA.SUBHEADING || "Your AI chat assistant";
 }
 
 async function get_config() {
@@ -17,3 +19,42 @@ async function get_config() {
     return resp.json();
 }
 
+// console.log(CONFIG_DATA,'CONFIG_DATA')
+function send_request(request_url, payload) {
+    try {
+        const base_url =
+            (CONFIG_DATA && CONFIG_DATA.BASE_API_URL) ||
+            "http://localhost:5000";
+        console.log('in request function',payload)
+        axios
+            .post(base_url.concat("/", request_url), payload, {
+                Headers: { "Access-Control-Allow-Credentials": true },
+            })
+            .then(function (response) {
+                // handle success
+                console.log(response);
+                return response, 0;
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+                return error, -1;
+            });
+    } catch (error) {
+        console.log(error);
+        return error, -1;
+    }
+}
+
+send_request("event-stream");
+
+document.querySelector("#submit_button").addEventListener("click",btn_press)
+
+function btn_press() {
+
+    let payload = {
+        input_prompt : document.querySelector("#prompt-text").value,
+    }
+
+    send_request('aiApiCall',payload)    
+}
